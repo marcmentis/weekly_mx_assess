2420777464cd3618b491a84f496c687f26bd7ba7
Marc Mentis <mmentis@optonline.net>
2015-09-30 15:13:20 -0400

    Calculate days in hosp for pdf

--- app/pdfs/mxa_tracker2_pdf.rb
+++ app/pdfs/mxa_tracker2_pdf.rb
@@ -9,19 +9,23 @@ class MxaTracker2Pdf < Prawn::Document
 
 	def heading	
 		# text "#{font_size.inspect}"	
-		text "Past Management Assessments", style: :bold
+		text "Past Management Assessments", style: :bold, align: :center
 		move_down 20
 	end
 
 	def body
-		font_size(10)
+		# Variables
+			font_size(10)
+			span_width = 500
 		# Get pat_demog variables
 
 		# firstname = @pat_demog[:firstname]
 		# lastname = @pat_demog[:lastname]
 		name = ''+@pat_demog[:lastname]+' '+@pat_demog[:firstname]+''
 		@pat_demog[:doa].blank? ? doa = "" : doa = @pat_demog[:doa].strftime("%F")
+		doa4diff = doa.to_date
 
+		text 'NAME: '+name+'    DOA: '+doa+'', style: :bold
 
 		@pat_assessments.each do |a|
 			# Get variables for current patient assessment
@@ -29,16 +33,22 @@ class MxaTracker2Pdf < Prawn::Document
 			a.updated_at.blank? ? updated_at = "" : updated_at = a.updated_at.strftime("%F")
 			a.pre_date.blank? ? pre_date = "" : pre_date = a.pre_date.strftime("%F")
 			# Put days in hosp here
-			days_in_hosp = '50'
+			meeting_date4diff = meeting_date.to_date
+			days_in_hosp_integer = (meeting_date4diff - doa4diff).to_i
+			days_in_hosp = days_in_hosp_integer.to_s
+
+
 			
 
 
 			move_down 10
 			stroke_horizontal_rule
-			pad_top(5) { text 'MEETING DATE: '+meeting_date+'' }
-			text "SAVED BY: "+a.updated_by+"   ON: "+updated_at+"   DAYS In HOSP: <color rgb='ff0000'>"+days_in_hosp+"</color>",
-				inline_format: true
-			text 'NAME: '+name+'    DOA: '+doa+''
+			pad_top(5) do
+			 	text "MEETING DATE: "+meeting_date+"      DAYS In HOSP: <color rgb='ff0000'>"+days_in_hosp+"</color>", 
+			 		inline_format: true
+			end
+			text "SAVED BY: "+a.updated_by+"   ON: "+updated_at+" "
+				
 
 			move_down 5
 			text 'PATIENT DANGEROUS (Self/Others) IF IN APPROVED HOUSING: '+a.danger_yn+''
@@ -46,17 +56,25 @@ class MxaTracker2Pdf < Prawn::Document
 			if a.danger_yn == 'Y'
 				text 'MEDS LAST CHANGED: '+a.drugs_last_changed+''
 				if a.drugs_last_changed == '0-8Weeks'
-					text a.drugs_change_why
+						span(span_width, :position => :center) do
+						text ''+a.drugs_change_why+''
+					end
 				elsif a.drugs_last_changed == 'Gt8Weeks'
-					text a.drugs_not_why
+					span(span_width, :position => :center) do
+						text a.drugs_not_why
+					end
 				end
 				
 			elsif a.danger_yn == 'N'
 				text 'Date set for Pre-Conference Meeting: '+a.pre_date_yesno+''
 				if a.pre_date_yesno == 'Y'
-					text 'Date: '+pre_date+''
+					span(span_width, :position => :center) do
+						text 'Date: '+pre_date+''
+					end
 				elsif a.pre_date_yesno == 'N'
-					text a.pre_date_no_why
+					span(span_width, :position => :center) do
+						text a.pre_date_no_why
+					end
 				end
 			end
 		end
